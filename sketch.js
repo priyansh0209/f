5bf6f49884763e6823fdfcd049c345be8b813361
priyansh0209 <81817605+priyansh0209@users.noreply.github.com>
2021-06-04 16:03:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,82 @@
+var dog,sadDog,happyDog, database;
+var foodS,foodStock;
+var addFood,feedDog;
+var foodObj;
+var gameState,readState;
+
+//create feed and lastFed variable here
+var feed,lastFed;
+
+
+function preload(){
+sadDog=loadImage("Dog.png");
+happyDog=loadImage("happy dog.png");
+}
+
+function setup() {
+  database=firebase.database();
+  createCanvas(1000,400);
+
+  foodObj = new Food();
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+
+   readState=database.ref('gameState');
+  readState.on("value",function(data){
+    gameState=data.val();
+  });
+  
+  dog=createSprite(800,200,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
+
+  //create feed the dog button here
+  feedDog=createButton("Feed The Dog")
+  feedDog.position(600,95);
+  feedDog.mousePressed(feedDog);
+
+  addFood=createButton("Add Food");
+  addFood.position(800,95);
+  addFood.mousePressed(addFoods);
+
+}
+
+function draw() {
+  background(46,139,87);
+  foodObj.display();
+   
+ drawSprites();
+}
+
+//function to read food Stock
+function readStock(data){
+  foodS=data.val();
+  foodObj.updateFoodStock(foodS);
+}
+
+
+function feedDog(){
+  dog.addImage(happyDog);
+  foodObj.updateFoodStock(foodObj.getFoodStock()-1);
+  database.ref('/').update({
+    Food:foodObj.getFoodStock(),
+    FeedTime:hour(),
+    gameState:"Hungry"
+  })
+  }
+
+//function to add food in stock
+function addFoods(){
+  console.log("Hi")
+  foodS++;
+  database.ref('/').update({
+    Food:foodS
+  })
+}
+
+function update(state){
+  database.ref('/').update({
+    gameState:state
+  })
+}
